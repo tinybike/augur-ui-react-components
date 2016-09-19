9e319daf58ce7ecf20beaccb355af9afbf717571
Stephen Sprinkle <git@sprinkle.io>
2016-09-19 11:35:58 -0600

    different approach to solve the unknown prop issue

--- src/modules/portfolio/components/positions.jsx
+++ src/modules/portfolio/components/positions.jsx
@@ -5,23 +5,28 @@ import Link from '../../link/components/link';
 
 const PortfolioPositions = (p) => (
 	<div className="positions-content" >
-		{!!p.markets && !!p.markets.length && p.markets.map(market => (
-			<Link key={market.id} href={market.marketLink.href} onClick={market.marketLink.onClick} >
-				<div className="positions-container" >
-					<PositionsMarketOverview
-						description={market.description}
-						{...market.myPositionsSummary}
-					/>
-					{!!market.myPositionOutcomes && !!market.myPositionOutcomes.length &&
+		{!!p.markets && !!p.markets.length && p.markets.map(positionMarket => {
+			const market = Object.assign({}, positionMarket);
+			delete market.marketLink.text;
+
+			return (
+				<Link key={market.id} {...market.marketLink} >
+					<div className="positions-container" >
+						<PositionsMarketOverview
+							description={market.description}
+							{...market.myPositionsSummary}
+						/>
+						{!!market.myPositionOutcomes && !!market.myPositionOutcomes.length &&
 						<Positions
 							className="page-content positions-content"
 							type={market.type}
 							outcomes={market.myPositionOutcomes}
 						/>
-					}
-				</div>
-			</Link>
-		))}
+						}
+					</div>
+				</Link>
+			);
+		})}
 	</div>
 );
 
