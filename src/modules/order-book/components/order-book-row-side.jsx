973db9808cc5223cc723679deb4659e2cc0be940
Stephen Sprinkle <git@sprinkle.io>
2016-11-22 07:34:40 -0700

    added tooltip

--- src/modules/order-book/components/order-book-row-side.jsx
+++ src/modules/order-book/components/order-book-row-side.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import ReactTooltip from 'react-tooltip';
 
 import ValueDenomination from 'modules/common/components/value-denomination';
 import NullStateMessage from 'modules/common/components/null-state-message';
@@ -8,7 +9,7 @@ import getValue from 'utils/get-value';
 import setShareDenomination from 'utils/set-share-denomination';
 
 import { BUY, SELL } from 'modules/outcomes/constants/trade-types';
-import { BID } from 'modules/transactions/constants/types';
+import { BID, ASK } from 'modules/transactions/constants/types';
 
 const OrderBookRowSide = (p) => {
 	const orders = getValue(p, 'orders');
@@ -23,14 +24,31 @@ const OrderBookRowSide = (p) => {
 					{(p.orders || []).map((order, i) => {
 						const shares = setShareDenomination(getValue(order, 'shares.formatted'), p.selectedShareDenomination);
 						const price = getValue(order, 'price.formatted');
+						const type = p.type || ASK;
 
 						return (
 							<div
 								key={i}
 								className={classNames('order-book-side-row not-selectable', { 'is-of-current-user': order.isOfCurrentUser })}
+								data-tip
+								data-for={`${type}-${i}-orders`}
+								data-event="click mouseenter"
+								data-event-off="click mouseleave"
 							>
 								<ValueDenomination formatted={p.type === BID ? shares : price} />
 								<ValueDenomination formatted={p.type === BID ? price : shares} />
+								{order.isOfCurrentUser &&
+									<ReactTooltip
+										id={`${type}-${i}-orders`}
+										type="info"
+										effect="solid"
+										place="top"
+									>
+										<span className="tooltip-text">
+											Your Order
+										</span>
+									</ReactTooltip>
+								}
 							</div>
 						);
 					})}
